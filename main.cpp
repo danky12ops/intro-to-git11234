5708b2d9d6cc521538e8dd69dfe760680528c828
danky12ops <danky.in.your.ass@gmail.com>
2026-01-17 17:22:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
-#include <cmath>
-
 using namespace std;
+bool checkArea(double x, double y) {
+    return (x >= 0 && y >= 0 && (x * x + y * y <= 1));
+}
+
 int main() {
     setlocale(LC_ALL, "uk_UA.utf8");
-
     double x, y;
-
-    cout << "--- Лабораторна робота №10. Варіант 25 ---" << endl;
-    cout << "Введіть координату X: ";
-    cin >> x;
-    cout << "Введіть координату Y: ";
-    cin >> y;
-    if (x >= 0 && y >= 0 && (x * x + y * y <= 1)) {
-        cout << "Результат: Точка потрапляє в заштриховану область." << endl;
+    cout << "--- Лабораторна №15 (Функції). Варіант 25 ---" << endl;
+    cout << "Введіть X та Y: ";
+    cin >> x >> y;
+    if (checkArea(x, y)) {
+        cout << "Точка в області" << endl;
     } else {
-        cout << "Результат: Точка НЕ потрапляє в область." << endl;
+        cout << "Точка поза областю" << endl;
     }
-
     return 0;
 }
